0595b436488d3c95a013bd7cb06129e570e319ae
Ricardo Acosta Barquero <ricardo.0709@hotmail.com>
2017-02-21 23:58:41 -0600

    CRUD para Cursos, listo.

--- MarianBakerSchoolDennisRicardoCicloIIIAnio2016/Data/CursoData.cs
+++ MarianBakerSchoolDennisRicardoCicloIIIAnio2016/Data/CursoData.cs
@@ -38,7 +38,143 @@ namespace Data
                 listaCursos.AddLast(curso);
             }
             connection.Close();
+            if (listaCursos.Count == 0)
+            {
+                return null;
+            }
             return listaCursos;
+        }
+
+        public Curso ObtenerCurso(string codigoCurso)
+        {
+            SqlConnection connection = new SqlConnection(stringConexion);
+            string sqlProcedureObtenerCurso = "sp_obtener_curso";
+            SqlCommand comandoObtenerCurso = new SqlCommand(sqlProcedureObtenerCurso, connection);
+            comandoObtenerCurso.CommandType = System.Data.CommandType.StoredProcedure;
+            comandoObtenerCurso.Parameters.Add(new SqlParameter("@codigo", codigoCurso));
+            connection.Open();
+            SqlDataReader dataReader = comandoObtenerCurso.ExecuteReader();
+            Curso curso = new Curso();
+            while (dataReader.Read())
+            {
+                curso.Codigo = dataReader["codigo"].ToString();
+                curso.Nombre = dataReader["nombreCurso"].ToString();
+                curso.Docente.Cedula = dataReader["cedulaDocente"].ToString();
+                curso.Docente.Nombre = dataReader["nombreDocente"].ToString();
+                curso.Docente.PrimerApellido = dataReader["primerApellido"].ToString();
+            }
+            connection.Close();
+            if (curso.Codigo == "")
+            {
+                return null;
+            }
+            return curso;
+        }
+
+        public void InsertarCurso(Curso curso)
+        {
+            string slqProcedureInsertarCurso = "sp_insertar_curso";
+            SqlConnection conexion = new SqlConnection(stringConexion);
+            SqlCommand cmdInsertarCurso = new SqlCommand(slqProcedureInsertarCurso, conexion);
+            cmdInsertarCurso.CommandType = System.Data.CommandType.StoredProcedure;
+
+            //Agregar los demas parametros restantes
+            cmdInsertarCurso.Parameters.Add(new SqlParameter("@codigo", curso.Codigo));
+            cmdInsertarCurso.Parameters.Add(new SqlParameter("@nombre", curso.Nombre));
+            cmdInsertarCurso.Parameters.Add(new SqlParameter("@cedulaDocente", curso.Docente.Cedula));
+            conexion.Open();
+            try
+            {
+                cmdInsertarCurso.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public void ActualizarCurso(Curso curso)
+        {
+            string slqProcedureActualizarCurso = "sp_actualizar_curso";
+            SqlConnection conexion = new SqlConnection(stringConexion);
+            SqlCommand cmdActualizarCurso = new SqlCommand(slqProcedureActualizarCurso, conexion);
+            cmdActualizarCurso.CommandType = System.Data.CommandType.StoredProcedure;
+            //Agregar los demas parametros restantes
+            cmdActualizarCurso.Parameters.Add(new SqlParameter("@codigo", curso.Codigo));
+            cmdActualizarCurso.Parameters.Add(new SqlParameter("@nombre", curso.Nombre));
+            cmdActualizarCurso.Parameters.Add(new SqlParameter("@cedulaDocente", curso.Docente.Cedula));
+            conexion.Open();
+            try
+            {
+                cmdActualizarCurso.ExecuteNonQuery();
+            }
+            catch (SqlException exc)
+            {
+                throw exc;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public void EliminarCurso(string codigoCurso)
+        {
+            string slqProcedureEliminarCurso = "sp_eliminar_curso";
+            SqlConnection conexion = new SqlConnection(stringConexion);
+            SqlCommand cmdEliminarCurso = new SqlCommand(slqProcedureEliminarCurso, conexion);
+            cmdEliminarCurso.CommandType = System.Data.CommandType.StoredProcedure;
+            //Agregar los demas parametros restantes
+            cmdEliminarCurso.Parameters.Add(new SqlParameter("@codigo", codigoCurso));
+            conexion.Open();
+            try
+            {
+                cmdEliminarCurso.ExecuteNonQuery();
+            }
+            catch (SqlException exc)
+            {
+                throw exc;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+
+        }
+
+        public string GenerarCodigoCurso()
+        {
+            //obtenemos el último código
+            SqlConnection connection = new SqlConnection(stringConexion);
+            string sqlProcedureObtenerCurso = "sp_obtener_ultimo_curso";
+            SqlCommand comandoObtenerCurso = new SqlCommand(sqlProcedureObtenerCurso, connection);
+            comandoObtenerCurso.CommandType = System.Data.CommandType.StoredProcedure;
+            connection.Open();
+            SqlDataReader dataReader = comandoObtenerCurso.ExecuteReader();
+            string codigo = "";
+
+            LinkedList<Curso> listaCursos = new LinkedList<Curso>();
+            while (dataReader.Read())
+            {
+                codigo = dataReader["codigo"].ToString();
+            }
+            if (codigo != "")
+            {
+                //ahora lo manipulamos para generar el siguiente
+                int numero = Int32.Parse(codigo.Remove(0, 6));
+                numero++;
+                codigo = "CURSO-" + numero;
+            }
+            else
+            {
+                codigo = "CURSO-1";
+            }
+            connection.Close();
+            return codigo;
         }
     }
 }
--- MarianBakerSchoolDennisRicardoCicloIIIAnio2016/Test/Data/CursoDataTest.cs
+++ MarianBakerSchoolDennisRicardoCicloIIIAnio2016/Test/Data/CursoDataTest.cs
@@ -18,14 +18,55 @@ namespace Test
             this.cursoData = new CursoData("Data Source=163.178.107.130;Initial Catalog=MariamBakerDB;Persist Security Info=True;User ID=sqlserver;Password=saucr.12");
         }
 
+        //[TestMethod]
+        //public void ObtenerCursos()
+        //{
+        //    LinkedList<Curso> listaCursos = this.cursoData.ObtenerCursos();
+        //    foreach (Curso cursoActual in listaCursos)
+        //    {
+        //        Console.Write(cursoActual.Codigo + " " + cursoActual.Nombre + " " + cursoActual.Docente.Nombre + " " + cursoActual.Docente.PrimerApellido + "\n");
+        //    }
+        //}
+
+        //[TestMethod]
+        //public void InsertarCurso()
+        //{
+        //    Curso curso = new Curso();
+        //    curso.Codigo = "CURSO-2";
+        //    curso.Nombre = "Español";
+        //    curso.Docente.Cedula = "112364526";
+        //    this.cursoData.InsertarCurso(curso);
+        //}
+
         [TestMethod]
-        public void ObtenerCursos()
+        public void GenerarCodigoCurso()
         {
-            LinkedList<Curso> listaCursos = this.cursoData.ObtenerCursos();
-            foreach (Curso cursoActual in listaCursos)
-            {
-                Console.Write(cursoActual.Codigo + " " + cursoActual.Nombre + " " + cursoActual.Docente.Nombre + " " + cursoActual.Docente.PrimerApellido);
-            }
+            Console.Write(this.cursoData.GenerarCodigoCurso());
         }
+
+        //[TestMethod]
+        //public void ObtenerCursos()
+        //{
+        //    Curso curso = this.cursoData.ObtenerCurso("CURSO-1");
+        //    Console.Write(curso.Codigo + " " + curso.Nombre + " " + curso.Docente.Nombre + " " + curso.Docente.PrimerApellido + "\n");
+        ////}
+
+        //[TestMethod]
+        //public void ActualizarCurso()
+        //{
+        //    Curso curso = new Curso();
+        //    curso.Codigo = "CURSO-2";
+        //    curso.Nombre = "Lengua Española";
+        //    curso.Docente.Cedula = "112364526";
+
+        //    this.cursoData.ActualizarCurso(curso);
+        ////}
+
+        ////[TestMethod]
+        ////public void EliminarCurso()
+        ////{
+        ////    string codigoCurso = "CURSO-1";
+        ////    this.cursoData.EliminarCurso(codigoCurso);
+        ////}
     }
 }
